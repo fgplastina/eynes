8dc435e8f112b9d9d904bffe5aec6ff39b02855b
Federico Plastina <40448913+FedericoFGP@users.noreply.github.com>
2023-03-13 19:03:52 -0300

    feat: simple exercise

---
+++ simple.py
@@ -0,0 +1,20 @@
+import random
+from operator import itemgetter
+
+
+def get_data():
+    data = [{'id': iterator, 'age': random.randrange(100)} for iterator in range(0,10)]
+    return data
+
+
+def order_data_by_age(data:list):
+    new_list = sorted(data, key=itemgetter('age'), reverse=True)
+    print(f'Youngest person in list {new_list[-1]}')
+    print('\n')
+    print(f'Oldest person in list {new_list[0]}')
+    return new_list
+
+order_data_by_age(get_data())
+
+
+
